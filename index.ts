60667878317afb562ae5f0740caf97ab7da4f1ad
liamdp <liam.pickering@outlook.com>
2020-07-22 23:30:29 +0100

    added initial code
    but in full working order (I think). Will add role based on reaction.

---
+++ index.ts
@@ -0,0 +1,82 @@
+//import { EventEmitter } from 'events';
+import { Client, RoleManager, GuildMember, ReactionCollector } from 'discord.js';
+import { Logger, LogLevel, LogLevelValue, ConsoleLogger } from './lib/logger';
+
+const logger = new ConsoleLogger(LogLevelValue.INFO);
+const client = new Client({ partials: ['MESSAGE', 'USER', 'REACTION'] });
+const listeningMessage = "message_id";
+
+client.on('ready', () => {
+
+    logger.log(`Online as ${client.user.tag}`, LogLevel.VERBOSE);
+    
+});
+
+client.on('messageReactionAdd', (messageReaction, user) => {
+    //let { id } = messageReaction.message; //why { id }
+
+    if(messageReaction.emoji.name === "✅") {
+
+        if (messageReaction.message.id === listeningMessage) {
+            
+            try {
+
+                logger.log(`Role added to ${user.username} (${user.id})`, LogLevel.INFO)
+                messageReaction.message.guild.member(user.id).roles.add(editRole(messageReaction));
+
+            } catch(err) {
+
+                logger.log("Role doesn't exist. Failed to return satisfactory result from editRole.", LogLevel.ERROR);
+
+            }
+            
+
+        }
+    
+    } else {
+        logger.log("Reaction added but it was not a tick.", LogLevel.INFO)
+    }
+
+});
+
+client.on('messageReactionRemove', (messageReaction, user) => {
+    //let { id } = messageReaction.message; //why { id }
+
+    if(messageReaction.emoji.name === "✅") {
+
+        if (messageReaction.message.id === listeningMessage) {
+            
+            try {
+
+                logger.log(`Role removed from ${user.username} (${user.id}).`, LogLevel.INFO)
+                messageReaction.message.guild.member(user.id).roles.remove(editRole(messageReaction));
+
+            } catch(err) {
+
+                logger.log("Role doesn't exist. Failed to return satisfactory result from editRole.", LogLevel.ERROR);
+
+            }
+            
+
+        }
+    
+    } else {
+        logger.log("Reaction removed but it was not a tick.", LogLevel.INFO)
+    }
+
+})
+
+const editRole = (reaction) => {
+
+    let roleName = 'Moffie';
+    let role = reaction.message.guild.roles.cache.find(x => x.name == roleName);
+    if (typeof role === undefined) {
+        logger.log(`Role ${roleName} does not exist!`, LogLevel.ERROR)
+    } else {
+        return role;
+    }
+
+}
+
+client.login('token');
+
---
+++ lib/logger.ts
@@ -0,0 +1,57 @@
+/*
+
+    Name: logger.ts
+    Version: 0.0.1
+    Author: Liam P with **loads** of help from Gavin v. G
+    Date: 21/07/2020
+
+*/
+
+export enum LogLevel {
+
+    INFO = "INFO",
+    WARN = "WARN",
+    ERROR = "ERROR",
+    FATAL = "FATAL",
+    VERBOSE = "VERBOSE"
+
+}
+
+export enum LogLevelValue {
+
+    INFO = 0,
+    WARN = 10,
+    ERROR = 20,
+    FATAL = 30,
+    VERBOSE = 100
+
+}
+
+export interface Logger {
+
+    log(message: string, level: LogLevel): this;
+
+}
+
+export class ConsoleLogger implements Logger {
+
+    constructor(private readonly state: LogLevelValue) {
+        
+    }
+
+    log(message: string, level: LogLevel): this {
+
+        if (LogLevelValue[level] >= this.state) {
+
+            console.log(`${new Date().toISOString()}  ${level} >> ${message}`)
+
+        }
+
+        return this;
+
+    }
+
+}
+
+// const logger = new ConsoleLogger(LogLevelValue.INFO);
+// logger.log("Hello", LogLevel.ERROR)
