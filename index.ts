0ed1ebcf6a9328ced4c7e792cbfe7e0846c73be0
liamdp <liam.pickering@outlook.com>
2020-07-24 00:06:43 +0100

    cleaned code, added more options to config

--- index.ts
+++ index.ts
@@ -1,86 +1,110 @@
-import { Client, Message } from 'discord.js';
-import { Logger, LogLevel, LogLevelValue, ConsoleLogger } from './lib/logger';
-import { token, listeningMessage, prefix } from './config.json';
+/**
 
-const logger:Logger = new ConsoleLogger(LogLevelValue.INFO);
+    Name: index.ts
+    Version: 1.0.1
+    Author: Liam P, Gavin v. G
+    Date: 21/07/2020
+    Description: Main bot code. Will handle commands, reactions and users joining the server.
+
+**/
+
+import { Client, Message, User, PartialUser } from 'discord.js';
+import { Logger, LogLevel, LogLevelValue, ConsoleLogger } from './lib/logger';
+import { token, listeningMessage, prefix, roleName, welcomeMsg, developers } from './config.json';
+
+const logger: Logger = new ConsoleLogger(LogLevelValue.INFO);
 const client = new Client({ partials: ['MESSAGE', 'USER', 'REACTION'] });
 
 client.on('ready', () => {
     logger.log(`Online as ${client.user.tag}`, LogLevel.VERBOSE);  
 });
 
-// REACTION TO ADD ROLE
+/**
+ *  Reaction to add role.
+ */
+client.on('messageReactionAdd', async(messageReaction, user) => {
 
-client.on('messageReactionAdd', (messageReaction, user) => {
-
-    if(messageReaction.emoji.name === "✅") {
+    if (messageReaction.emoji.name === '✅') {
 
         if (messageReaction.message.id === listeningMessage) {
             
             try {
-                logger.log(`Role added to ${user.username} (${user.id})`, LogLevel.INFO)
-                messageReaction.message.guild.member(user.id).roles.add(editRole(messageReaction));
-            } catch(err) {
-                logger.log("Role doesn't exist. Failed to return satisfactory result from editRole.", LogLevel.ERROR);
+                const { member, role } = extractMessageProps(messageReaction.message, user, roleName);
+                await member.roles.add(role);
+                logger.log(`Role added to ${user.username} (${user.id})`, LogLevel.INFO);
+            } catch (err) {
+                logger.log(`Role ${roleName} doesn't exist.`, LogLevel.ERROR);
             }  
 
         } 
     } else {
-        logger.log("Reaction added but it was not a tick.", LogLevel.INFO)
+        logger.log('Reaction added but it was not a tick.', LogLevel.INFO);
     }
 });
 
-client.on('messageReactionRemove', (messageReaction, user) => {
+/**
+ *  Reaction to remove role.
+ */
+client.on('messageReactionRemove', async(messageReaction, user) => {
 
-    if(messageReaction.emoji.name === "✅") {
+    if (messageReaction.emoji.name === '✅') {
 
         if (messageReaction.message.id === listeningMessage) {
             
             try {
-                logger.log(`Role removed from ${user.username} (${user.id}).`, LogLevel.INFO)
-                messageReaction.message.guild.member(user.id).roles.remove(editRole(messageReaction));
-            } catch(err) {
-                logger.log("Role doesn't exist. Failed to return satisfactory result from editRole.", LogLevel.ERROR);
+                const { member, role } = extractMessageProps(messageReaction.message, user, roleName); 
+                await member.roles.remove(role);
+                logger.log(`Role removed from ${user.username} (${user.id}).`, LogLevel.INFO);
+            } catch (err) {
+                logger.log(`Role ${roleName} doesn't exist.`, LogLevel.ERROR);
             }      
 
         }
     } else {
-        logger.log("Reaction removed but it was not a tick.", LogLevel.INFO)
+        logger.log('Reaction removed but it was not a tick.', LogLevel.INFO);
     }
 });
 
-const editRole = (reaction) => {
-    let roleName = 'Moffie';
-    let role = reaction.message.guild.roles.cache.find(x => x.name == roleName);
-    if (typeof role === undefined) {
-        logger.log(`Role ${roleName} does not exist!`, LogLevel.ERROR)
-    } else {
-        return role;
-    }
+/**
+ * Grabs member that reacted to a message and the role that will be assigned to them.
+ * @param message Message that was reacted to.
+ * @param user User that reacted.
+ * @param roleName Name of the role we want to add to the user that reacted.
+ */
+const extractMessageProps = (message: Message, user: User | PartialUser, roleName: string) => {
+
+    return {
+        member: message.guild.member(user.id),
+        role: message.guild.roles.cache.find(x => x.name === roleName)
+    };
+
 };
 
-// WELCOME A USER
-
+/**
+ * Welcomes a user by sending them a private message.
+ */
 client.on('guildMemberAdd', member => {
+    member.send(welcomeMsg);
     logger.log(`${member} has joined! The welcome info was sent.`, LogLevel.INFO);
-    member.send("Test");
 });
 
-// DEV FOR WELCOMING A USER
+/**
+ * Developer version for sending a welcome message. Will fire if a developer sends the command ¬testwelcome.
+ */
 client.on('message', (msg: Message) => {
 
-    const allowedUsers = ['574333819017101312', '167764605957832704'];
+    const allowedUsers = developers;
     const msgMatchesPrefix = !msg.content.indexOf(prefix);
     const authorIsAllowed = allowedUsers.includes(msg.author.id);
 
     if (!msgMatchesPrefix || !authorIsAllowed) return;
 
-	const args = msg.content.slice(prefix.length).trim().split(/ +/);
+    const args = msg.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
     
     if (command === 'testwelcome') {
-        msg.author.send("Test worked.");
-        logger.log(`Message sent to ${msg.author.username} (${msg.author.id}).`, LogLevel.INFO);
+        msg.author.send(welcomeMsg);
+        logger.log(`Welcome message sent to ${msg.author.username} (${msg.author.id}).`, LogLevel.INFO);
     } else {
         return;
     }
